259246c86b76e4e70e13b9a58e114a9794d1c42f
adimsanjay <adimsanjay1312@gmail.com>
2021-10-16 14:20:11 +0530

    js/service page updated

--- js/services.js
+++ js/services.js
@@ -1,32 +1,164 @@
-var services = [
-    {
-        id:"001",
-        servicename:'Fabrication Work',
-        work = [
-            {
-                worksub:"Industrial PFB Shel",
+var services = [{
+        id: "001",
+        servicename: 'Fabrication Work',
+        work = [{
+                worksub: "Industrial PFB Sheel",
             },
             {
-                worksub:"Enginer work",
+                worksub: "Engineering work",
             },
             {
-                worksub:"Custom [Ms/ss]",
+                worksub: "Custom [Ms/ss]",
             },
         ]
     },
     {
-        id:"001",
-        servicename:'Fabrication Work',
-        work = [
-            {
-                worksub:"Industrial PFB Shel",
+        id: "002",
+        servicename: 'Industrial Flooring',
+        work = [{
+                worksub: "Epoxy Flooring",
             },
             {
-                worksub:"Enginer work",
+                worksub: "E.S.D Flooring",
             },
             {
-                worksub:"Custom [Ms/ss]",
+                worksub: "Official Carpet",
+            },
+            {
+                worksub: "Rubber Matting",
             },
         ]
+    },
+    {
+        id: "003",
+        servicename: 'Ceiling Work',
+        work = [{
+                worksub: "Armstrong 2*2 gride tailceiling",
+            },
+            {
+                worksub: "Gyproc Gypsum false ceiling",
+            },
+            {
+                worksub: "Sounel Proof Ceiling",
+            },
+        ]
+    },
+    {
+        id: "004",
+        servicename: 'Industrial Pipeline',
+        work = [{
+                worksub: "M.S. Pipeing",
+            },
+            {
+                worksub: "S.S. Pipeing",
+            },
+            {
+                worksub: "Haydrant Sprinklar System",
+            },
+            {
+                worksub: "PPRC Pipeing",
+            },
+            {
+                worksub: "HDPC Pipeing",
+            },
+            {
+                worksub: "PVC Pipeing",
+            },
+            {
+                worksub: "UPVC Pipeing",
+            },
+        ]
+    },
+    {
+        id: "005",
+        servicename: 'Civil Work',
+        work = [{
+            worksub: "Water Proofing Work",
+        }, ]
+    },
+    {
+        id: "006",
+        servicename: 'Puff Panel Work',
+        work = [{
+                worksub: "Cleen room puff panel work",
+            },
+            {
+                worksub: "Fire proof puff panel work",
+            },
+            {
+                worksub: "Puff panel Ex.Side Shed",
+            },
+            {
+                worksub: "Puff Panel Wall",
+            },
+            {
+                worksub: "Puff Panel Roofing",
+            },
+        ]
+    },
+    {
+        id: "007",
+        servicename: 'Industrial Partition Work',
+        work = [{
+                worksub: "Aluminium Partition Work",
+            },
+            {
+                worksub: "Gypsum board partitionwork",
+            },
+            {
+                worksub: "Puff Panel Partition",
+            },
+            {
+                worksub: "Interior Wood Partition",
+            },
+            {
+                worksub: "Sound Proof Partition",
+            },
+            {
+                worksub: "Water Proof Partition",
+            },
+        ]
+    },
+    {
+        id: "008",
+        servicename: 'AC AHU Ducting',
+        work = [{
+            worksub: "Colling AC AHU Ducting",
+        }, ]
+    },
+    {
+        id: "009",
+        servicename: 'Glass Work',
+        work = [{
+                worksub: "Tuffin glass work",
+            },
+            {
+                worksub: "Slayding window work",
+            },
+            {
+                worksub: "Aluminium Glass Partition Work",
+            },
+        ]
+    },
+    {
+        id: "010",
+        servicename: 'Painting Work',
+        work = [{
+                worksub: "Ex.In. wall Painting",
+            },
+            {
+                worksub: "Polish Painting",
+            },
+            {
+                worksub: "Spray Painting",
+            },
+        ]
+    },
+    {
+        id: "011",
+        servicename: 'Office Furniture',
+        work = [{
+            worksub: "Interior 3D official plan with furniture,P.O.P Celling partition,painting flooring(Odetorium hall,meeting room,Boss cabin,Admin hall,)",
+        }, ]
     },
 ]
