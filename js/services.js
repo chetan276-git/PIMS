9a1af4acefa8f833ea708471d96b3ec79109ebcc
chetan mane <73518916+chetan276-git@users.noreply.github.com>
2021-10-16 12:26:32 +0530

    Services file added

---
+++ js/services.js
@@ -0,0 +1,32 @@
+var services = [
+    {
+        id:"001",
+        servicename:'Fabrication Work',
+        work = [
+            {
+                worksub:"Industrial PFB Shel",
+            },
+            {
+                worksub:"Enginer work",
+            },
+            {
+                worksub:"Custom [Ms/ss]",
+            },
+        ]
+    },
+    {
+        id:"001",
+        servicename:'Fabrication Work',
+        work = [
+            {
+                worksub:"Industrial PFB Shel",
+            },
+            {
+                worksub:"Enginer work",
+            },
+            {
+                worksub:"Custom [Ms/ss]",
+            },
+        ]
+    },
+]
